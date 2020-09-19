baffefb6a0c0ce4856bd30405df57693b2029fd7
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-09-19 19:58:44 -0300

    Setting up routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListCustomersComponent } from './list-customers/list-customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customers', component: ListCustomersComponent },
+  { path: '', redirectTo: 'customers', pathMatch: 'full' },
+  { path: '**', redirectTo: 'customers' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
